5e6d9ecc2f9dd13f01479f3dc4c868b5ceb983b1
Benjamin Georgeault <benjamin@drosalys.fr>
2021-07-21 14:42:24 +0200

    Use reactive form for login form.

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Router} from '@angular/router';
+import {Token} from './token';
+import {Credentials} from './credentials';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,10 +12,17 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  public email = '';
-  public password = '';
+  /**
+   * Build a new FormControl using Angular's FormBuilder.
+   * Add 2 attributes (email and password), both with an empty string as default values and with
+   */
+  public form: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
   constructor(
+    private fb: FormBuilder,
     private httpClient: HttpClient,
     private router: Router,
   ) { }
@@ -21,18 +31,25 @@ export class LoginComponent implements OnInit {
   }
 
   submit(): void {
-    if (
-      0 !== this.email.trim().length &&
-      0 !== this.password.trim().length
-    ) {
-      this.httpClient.post<{token: string}>('https://localhost:8000/authentication_token', {
-        email: this.email,
-        password: this.password,
-      }).subscribe((data) => {
-        localStorage.setItem('access_token', data.token);
-        // connected
-        this.router.navigate(['']);
-      });
+    // Set all inputs as touched (display errors of email and password when direct click on submit button).
+    this.form.markAllAsTouched();
+
+    // If the form is valid (all inputs valids).
+    if (this.form.valid) {
+      // Forge HTTP request to send to the API to retrieve JWT.
+      this.httpClient.post<Token>('https://localhost:8000/authentication_token', this.form.value as Credentials).subscribe(
+        (data) => {
+          // When success. Save the JWT in local storage.
+          localStorage.setItem('access_token', data.token);
+
+          // Then redirect Angular page to home.
+          this.router.navigate(['']);
+        },
+        (e: {error: {code: number, message: string}}) => {
+          // When error.
+          alert(e.error.message);
+        },
+      );
     }
   }
 }
